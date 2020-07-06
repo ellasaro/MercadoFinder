bdb9259a16431bd04f2dd5126814f6d4a05c7086
Miguel Lasa <el-lasa@hotmail.com>
2020-07-05 22:57:00 -0300

    First pull.

---
+++ app/build/generated/source/buildConfig/debug/com/blackfrogweb/mercadofinder/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.blackfrogweb.mercadofinder;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.blackfrogweb.mercadofinder";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
